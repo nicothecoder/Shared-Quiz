5e11a89ab61976c4e537b64e991b527d109962d0
nico_ <62893173+nicothecoder@users.noreply.github.com>
2020-12-12 14:08:47 +0000

    First update

--- Program.cs
+++ Program.cs
@@ -9,20 +9,31 @@ namespace ConsoleApp1
             Console.WriteLine("What's your name?");
             string input = Console.ReadLine();
             Console.WriteLine($"Hello {input}!");
-            /* This part will tell the user their age in a string */
+            // This part will tell the user their age in a string
             Console.WriteLine("How old are you?");
             double age = Convert.ToInt32(Console.ReadLine());
+            // This part tells you if you can drive based on your age
             Console.WriteLine($"You are {age} years old");
-            if (age < 17) 
+            // Young person
+            if (age < 17)
             {
-                Console.WriteLine("You are not allowed to drive");
+                Console.WriteLine("You are not allowed to drive!");
             }
-            else;
+            // Person aged 17
+            else if (age == 17) 
             {
-                Console.WriteLine("");
+                Console.WriteLine("Well done! You are now allowed to drive!");
             }
-            Console.WriteLine("Next Part....");
-            Console.WriteLine("Next.");
+            // Person aged over 17
+            else
+            {
+                Console.WriteLine("You are allowed to drive!");
+            }
+            Console.WriteLine("");
+            if
+            {
+
+            }
         }
     }
 }
